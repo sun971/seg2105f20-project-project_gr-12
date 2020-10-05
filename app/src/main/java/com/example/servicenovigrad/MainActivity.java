69af29c316d988ecd33dcd990896b0d7697ae5f1
Sun971 <aisorya.bhandari@gmail.com>
2020-10-05 18:38:17 -0400

    Updating MainActivity Page

--- app/src/main/java/com/example/servicenovigrad/MainActivity.java
+++ app/src/main/java/com/example/servicenovigrad/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static com.example.servicenovigrad.R.layout.activity_main;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
     }
 }
