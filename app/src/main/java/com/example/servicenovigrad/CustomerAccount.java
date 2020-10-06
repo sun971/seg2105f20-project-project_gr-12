75db20f1bdb689d259cf90077bb57e50ed52e024
Marshall Steele <marshallsteele123@hotmail.com>
2020-10-06 16:34:23 -0400

    Changed startup activity and added account classes

---
+++ app/src/main/java/com/example/servicenovigrad/CustomerAccount.java
@@ -0,0 +1,7 @@
+package com.example.servicenovigrad;
+
+public class CustomerAccount extends Account {
+    CustomerAccount(String firstName, String lastName, String eMail, String password) {
+        super(firstName, lastName, eMail, password);
+    }
+}
