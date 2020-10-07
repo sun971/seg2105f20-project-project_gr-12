95469744f71349df79ea810da674f6aa638a289c
Angus Leung <angusleung168@gmail.com>
2020-10-06 20:55:15 -0400

    Added login authentication

--- app/src/main/java/com/example/servicenovigrad/Login.java
+++ app/src/main/java/com/example/servicenovigrad/Login.java
@@ -1,18 +1,96 @@
 package com.example.servicenovigrad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Auth
+        session = FirebaseAuth.getInstance();
+    }
+
+    // JUST TESTING IF CAN READ IF USER IS LOGGED IN OR NOT
+    public void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = session.getCurrentUser();
+
+        if(currentUser != null)
+        {
+            Log.d("Session","User logged in: "+ currentUser);
+            //Toast.makeText(Login.this, "USER LOGGED IN", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Log.d("Session","User NOT logged in");
+            //Toast.makeText(Login.this, "USER NOT LOGGED IN", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+    public void login(View view) {
+
+            EditText emailField = (EditText)findViewById(R.id.email);
+            EditText passwordField = (EditText)findViewById(R.id.password);
+
+            String email = emailField.getText().toString();
+            String password = passwordField.getText().toString();
+
+            if(email.equals("") || email.equals(null) || password.equals("") || password.equals(null))
+            {
+                Toast.makeText(Login.this, "One or more fields has no value.", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Log.d("Email", email);
+                Log.d("Password", password);
+
+                session.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Msg", "signInWithEmail:success");
+                            FirebaseUser user = session.getCurrentUser();
+                            Log.d("Session",user.toString());
+
+                            Toast.makeText(Login.this, "Authentication success.", Toast.LENGTH_LONG).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Error", "COULD NOT SIGN IN", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+                            // ...
+                        }
+
+                        // ...
+                    }
+                });
+            }
+
+
+
     }
 
     public void goToSignupPage(View view)
