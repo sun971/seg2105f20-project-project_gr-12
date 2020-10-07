6da78e5d7d1258cd8f2a27b1aa7d471b0c7ba84c
Marshall Steele <marshallsteele123@hotmail.com>
2020-10-06 21:14:16 -0400

    Added Firebase integration for signup

--- app/src/main/java/com/example/servicenovigrad/Signup.java
+++ app/src/main/java/com/example/servicenovigrad/Signup.java
@@ -1,5 +1,6 @@
 package com.example.servicenovigrad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,20 +12,28 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Signup extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+    FirebaseAuth mAuth;
+    FirebaseDatabase mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
     }
 
     public void onStart()   {
@@ -40,6 +49,8 @@ public class Signup extends AppCompatActivity {
     }
 
     public void onClickSignup(View view) {
+        boolean validCredentials = true;
+
         EditText firstNameField = (EditText)findViewById(R.id.signupFirstName);
         EditText lastNameField = (EditText)findViewById(R.id.signupLastName);
         EditText usernameField = (EditText)findViewById(R.id.signupUsername);
@@ -50,16 +61,18 @@ public class Signup extends AppCompatActivity {
 
 
 
+
         if (firstNameField == null || lastNameField == null || usernameField == null || emailField == null || passwordField == null) {
             //They didnt enter a field.... sned toast notif
+            validCredentials = false;
         }
 
         String firstName = firstNameField.getText().toString();
         String lastName = lastNameField.getText().toString();
-        String userName = usernameField.getText().toString();
+        String username = usernameField.getText().toString();
         String email = emailField.getText().toString();
         String password = passwordField.getText().toString();
-        String accountType;
+        String accountType = "";
 
         switch (checkedId) {
             case R.id.employeeAccountSelect:
@@ -70,32 +83,60 @@ public class Signup extends AppCompatActivity {
                 break;
             default:
                 //send toast notif
+                validCredentials = false;
         }
 
-
+        //Attempt to create firebase user
         mAuth.createUserWithEmailAndPassword(email, password)
-                .then((auth: firebase.auth.Auth) => { return auth; } )
-                .catch((error: Error) => {
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
 
-            let authError = error as firebase.auth.Error;
-            let errorCode = authError.code;
-            let errorMessage = authError.message;
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()) {
+                            try {
+                                throw task.getException();
+                            } catch(FirebaseAuthWeakPasswordException e) {
+                                //Toast for weak password
+                            } catch(FirebaseAuthInvalidCredentialsException e) {
+                                //Toast for invalid email
+                            } catch(FirebaseAuthUserCollisionException e) {
+                                //Toast for existing user with that email
+                            } catch(Exception e) {
+                                //Toast for general user auth error
+                            }
+                        }
+                    }
+                });
 
-            if (errorMessage === "auth/weak-password") {
-                alert("The password is too weak.");
-            } else {
-                alert(errorMessage);
-            }
-            console.log(error);
-        });
+        FirebaseUser user = mAuth.getCurrentUser();
 
+        if (user == null) {
+            //Was unable to authenticate
+            validCredentials = false;
+        }
 
-        //SEND CREDENTIALS TO REALTIME DATABASE
+        //If account successfully created
+        if (validCredentials) {
+            //SEND CREDENTIALS TO REALTIME DATABASE
+            //return user to login page
+            String userId = user.getUid();
+            DatabaseReference newUserFirstNameRef = mDatabase.getReference("users/" + userId + "/firstName");
+            DatabaseReference newUserLastNameRef = mDatabase.getReference("users/" + userId + "/lastName");
+            DatabaseReference newUserEmailRef = mDatabase.getReference("users/" + userId + "/email");
+            DatabaseReference newUserUsernameRef = mDatabase.getReference("users/" + userId + "/username");
+            DatabaseReference newUserPasswordRef = mDatabase.getReference("users/" + userId + "/password");
+            DatabaseReference newUserAccountTypeRef = mDatabase.getReference("users/" + userId + "/accountType");
 
-        Log.d("First Name", firstName);
-        Log.d("Last Name", lastName);
-        Log.d("Username",userName);
-        Log.d("Email",email);
-        Log.d("Password",password);
+            newUserFirstNameRef.setValue(firstName);
+            newUserLastNameRef.setValue(lastName);
+            newUserEmailRef.setValue(email);
+            newUserUsernameRef.setValue(username);
+            newUserPasswordRef.setValue(password);
+            newUserAccountTypeRef.setValue(accountType);
+
+            Intent loginIntent = new Intent(this, Login.class);
+
+            startActivity(loginIntent);
+        }
     }
 }
