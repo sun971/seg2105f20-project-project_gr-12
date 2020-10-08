f9e76a4e4e9fad635fd4bc85461e9cf8a120e49d
Angus Leung <angusleung168@gmail.com>
2020-10-07 20:33:54 -0400

    Added variable values to welcome page data getter

--- app/src/main/java/com/example/servicenovigrad/WelcomePage.java
+++ app/src/main/java/com/example/servicenovigrad/WelcomePage.java
@@ -1,5 +1,6 @@
 package com.example.servicenovigrad;
 
+        import androidx.annotation.NonNull;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.content.Intent;
@@ -12,8 +13,11 @@ package com.example.servicenovigrad;
 //import com.google.firebase.database.FirebaseDatabase;
         import com.google.firebase.auth.FirebaseAuth;
         import com.google.firebase.auth.FirebaseUser;
+        import com.google.firebase.database.DataSnapshot;
+        import com.google.firebase.database.DatabaseError;
         import com.google.firebase.database.DatabaseReference;
         import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.database.ValueEventListener;
 
 public class WelcomePage extends AppCompatActivity {
     private FirebaseAuth session;
@@ -25,8 +29,8 @@ public class WelcomePage extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_page);
 
         session = FirebaseAuth.getInstance();
-        //final TextView helloTextView = (TextView) findViewById(R.id.welcomeMessage);
-        //helloTextView.setText(getCurrentUser());
+        mDatabase = FirebaseDatabase.getInstance();
+        getCurrentUser();
     }
 
     public void onStart()   {
@@ -41,7 +45,7 @@ public class WelcomePage extends AppCompatActivity {
 
 
         }
-        session.signOut();
+        //session.signOut();
     }
 
 
@@ -52,18 +56,32 @@ public class WelcomePage extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public String getCurrentUser(){
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    public void getCurrentUser(){
+        FirebaseUser user = session.getCurrentUser();
         if (user != null) {
             String id = user.getUid();
 
             //String firstName = user.getDisplayName();
+            String snap;
+            DatabaseReference userData = mDatabase.getReference("users/" + id);
 
-            DatabaseReference firstName = mDatabase.getReference("users/" + id + "/firstName");
-            DatabaseReference role = mDatabase.getReference("users/" + id + "/accountType");
+            userData.addListenerForSingleValueEvent(
+                    new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-            String str = "Welcome "+firstName.toString()+"! You are logged in as "+role.toString();
-            return str;
+                            TextView helloTextView = (TextView) findViewById(R.id.welcomeMessage);
+                            helloTextView.setText("Welcome "+snapshot.child("firstName").getValue()+", you are logged in as "+snapshot.child("accountType").getValue());
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    }
+
+            );
 
             //String role = user.
 
@@ -72,9 +90,11 @@ public class WelcomePage extends AppCompatActivity {
             // User is signed in
         } else {
             // No user is signed in
-            return null;
+            //return "Nothing";
         }
     }
+
+
 
 
 }
