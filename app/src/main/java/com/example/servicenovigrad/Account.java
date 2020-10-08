bdc7d4473c1783cafc7481dcce816bdea0564654
Marshall Steele <marshallsteele123@hotmail.com>
2020-10-07 21:18:23 -0400

    Added back account types

--- app/src/main/java/com/example/servicenovigrad/Account.java
+++ app/src/main/java/com/example/servicenovigrad/Account.java
@@ -1,6 +1,6 @@
 package com.example.servicenovigrad;
 
-public class Account {
+public abstract class Account {
     private String eMail;
     private String firstName;
     private String lastName;
@@ -9,6 +9,39 @@ public class Account {
         this.eMail = eMail;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.password = password;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public abstract String getAccountType();
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
--- app/src/main/java/com/example/servicenovigrad/EmployeeAccount.java
+++ app/src/main/java/com/example/servicenovigrad/EmployeeAccount.java
@@ -4,4 +4,7 @@ public class EmployeeAccount extends Account {
     EmployeeAccount(String firstName, String lastName, String eMail, String password) {
         super(firstName, lastName, eMail, password);
     }
+    public String getAccountType() {
+        return "employee";
+    }
 }
