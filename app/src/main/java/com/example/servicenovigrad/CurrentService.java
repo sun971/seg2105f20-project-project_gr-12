a2725cacc0056cef18e7759a5941c2c29bf9dc8c
Sun971 <aisorya.bhandari@gmail.com>
2020-10-29 18:18:10 -0400

    Set up class and will be used to show list of services

--- app/src/main/java/com/example/servicenovigrad/CurrentService.java
+++ app/src/main/java/com/example/servicenovigrad/CurrentService.java
@@ -2,7 +2,9 @@ package com.example.servicenovigrad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CurrentService extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class CurrentService extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_service);
+    }
+
+    public void CreateNewServices(View view) {
+        Intent intent = new Intent(this, CreateNewServices.class);
+        //add in the manage services view
+        startActivity(intent);
     }
 }
